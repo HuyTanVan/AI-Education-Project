d037249926f6613f13cbcaac973cbfd3ee75f21c
Hoang Huy Nguyen <huyhandsome189@gmail.com>
2024-09-16 13:37:22 -0400

    added Enrollment model

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@
 
 from . import db
 import uuid
-
+from datetime import datetime
 # auto-generated last 12 digits of uuid4
 def generate_uuid4_last12():
     return str(uuid.uuid4())[-12:]
@@ -13,9 +13,15 @@ class User(db.Model):
 
     Attributes:
         id (str): The unique identifier for the user.
-        username (str): The user's username (unique).
+        user_name (str): The user's username (unique).
+        first_name (str): The user's first name.
+        last_name (str): The user's last name.
+        email (str): The user's email (unique).
         password_hash (str): The hashed password for the user.
-        role (str): The role of the user ('student' or 'teacher').
+        role (str): The role of the user ('student', 'teacher' or 'admin').
+        last_login (DateTime): The last time user logged in.
+        created_at (DateTime): when the user account was created.
+        updated_at (DateTime): The last time the user’s information was updated.
     """
     id = db.Column(db.String(12), primary_key=True, default=generate_uuid4_last12, unique=True, nullable=False)
     user_name = db.Column(db.String(80), unique=True, nullable=False)
@@ -25,7 +31,7 @@ class User(db.Model):
     password_hash = db.Column(db.String(120), nullable=False)
     role = db.Column(db.String(10))  # 'student' or 'teacher'
     last_login = db.Column(db.DateTime, nullable = True)
-    created_at = db.Column(db.DateTime, nullable = False)
+    created_at = db.Column(db.DateTime, nullable = False, default=datetime.now)
     updated_at =  db.Column(db.DateTime, nullable = True)
 
     def __repr__(self) -> str:
@@ -43,8 +49,31 @@ class Course(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     course_name = db.Column(db.String(120), nullable=False)
-    teacher_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    teacher_id = db.Column(db.String(12), db.ForeignKey('user.id'))
+
     teacher = db.relationship('User', backref='courses')
+
+class Enrollment(db.Model):
+    """
+    Represents a course in the system.
+
+    Attributes:
+        id (int): The unique identifier for the enrollment.
+        student_id (int): The ID of the student.
+        course_id (int): The ID of the course.
+        status (str) : The status of current enrollment ('enrolled', 'cancelled', 'pending')        
+        enrollment_date (DateTime): When enrollment was made.
+        student (User): The relationship to the User model for the student.
+        course (Course): The relationship to the Course model for the course.
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.String(12), db.ForeignKey('user.id'))
+    course_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    status = db.Column(db.Enum('enrolled', 'cancelled', 'pending'),default='pending', nullable=False)
+    enrollment_date = db.Column(db.DateTime,default=datetime.now, nullable = True)
+
+    student = db.relationship('User', backref='courses')
+    course = db.relationship('Course', backref='courses')
 
 class Exam(db.Model):
     """
@@ -59,6 +88,7 @@ class Exam(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
     exam_title = db.Column(db.String(120), nullable=False)
+    exam_description = db.Column(db.String(255), nullable=True)
     course = db.relationship('Course', backref='exams')
 
 class Question(db.Model):
@@ -90,7 +120,7 @@ class Answer(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
-    student_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    student_id = db.Column(db.String(12), db.ForeignKey('user.id'))
     answer_text = db.Column(db.Text, nullable=False)
     question = db.relationship('Question', backref='answers')
     student = db.relationship('User', backref='answers')
